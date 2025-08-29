016f3d50bdf5ab0489906cadf0df78c508068c73
WaTh <info@wath.dev>
2025-08-29 13:42:49 +0200

    Add support for more linux distros

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define required packages
-PACKAGES="tlp python3-pyqt5 tlp acpi-call-dkms tlp tlp-rdw upower"
+PACKAGES="tlp python3-pyqt5 acpi-call-dkms tlp-rdw upower"
 
 # Detect the Linux distribution
 if [ -f /etc/os-release ]; then
@@ -17,21 +17,25 @@ echo "Installing required dependencies..."
 
 # Install dependencies based on the package manager
 case $OS in
-    ubuntu|debian)
+    ubuntu|debian|linuxmint|pop|elementary|zorin)
         sudo apt update
         sudo apt install -y $PACKAGES
         ;;
-    arch|manjaro)
+    arch|manjaro|cachyos|endeavouros|garuda)
+        # For Arch-based distros, acpi-call-dkms might be in the AUR
+        # This script assumes it's in the official repositories or a user-configured repo.
         sudo pacman -Syu --noconfirm $PACKAGES
         ;;
-    fedora)
+    fedora|nobara|rhel|centos|rocky|almalinux)
         sudo dnf install -y $PACKAGES
         ;;
-    opensuse|sles)
+    opensuse|sles|opensuse-tumbleweed)
         sudo zypper install -y $PACKAGES
         ;;
     *)
         echo "Unsupported Linux distribution: $OS"
+        echo "You may need to install the following packages manually for your distribution:"
+        echo "$PACKAGES"
         exit 1
         ;;
 esac
